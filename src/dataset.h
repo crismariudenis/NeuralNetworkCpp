5bebf8d6a39ba1b6b61e5e0f4e260fafe75bf212
crismariudenis <78813212+crismariudenis@users.noreply.github.com>
2023-07-27 22:24:08 +0300

    changed to sum of 2 3bit numbers

--- src/dataset.h
+++ src/dataset.h
@@ -76,10 +76,29 @@ namespace nn
                 {0, 0, 0},
             };
 
-            for (const auto &x : t)
+            // for (const auto &x : t)
+            // {
+            //     std::vector<T> input{x[0], x[1]};
+            //     std::vector<T> output{x[2]};
+            //     this->addData(input, output);
+            // }
+            for (int i = 0; i < 10; i++)
             {
-                std::vector<T> input{x[0], x[1]};
-                std::vector<T> output{x[2]};
+                int a = rand() % 8;
+                int b = rand() % 8;
+                std::vector<T> input;
+                // add a and b 's bits
+
+                for (int j = 0; j < 3; j++)
+                    input.push_back((a >> j) & 1);
+                    
+                for (int j = 0; j < 3; j++)
+                    input.push_back((b >> j) & 1);
+
+                std::vector<T> output;
+                // add a + b 's bits
+                for (int j = 0; j < 4; j++)
+                    output.push_back(((a + b) >> j) & 1);
                 this->addData(input, output);
             }
         }
@@ -106,7 +125,6 @@ namespace nn
             for (size_t j = 0; j < data.size(); j++, std::cout << '\n')
                 for (size_t i = 0; i < data[j].input.size(); i++)
                 {
-
                     std::cout << data[j].input[i];
                 }
         }
