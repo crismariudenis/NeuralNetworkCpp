def61b970f350bff368c92ec7e9253bdd692a07b
crismariudenis <78813212+crismariudenis@users.noreply.github.com>
2023-08-24 21:38:29 +0300

    cleaned todo's

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 
 int main()
 {
-    // Todo: Upgrade from finate diff function
     // Todo: Log all the network and convert from logs to code
     // Todo: Add the window subcrass to the gym to make it more "structured?"
-    // Todo: Make data in Matrix private
     // Todo: Multiple activation functions
 }
