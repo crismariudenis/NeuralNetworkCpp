eb918acacde2bd63d9c8a61da8fe732c4d6ac570
crismariudenis <78813212+crismariudenis@users.noreply.github.com>
2023-08-26 20:50:28 +0300

    digit Learn

---
+++ demos/digitLearn.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include "../include/raylib.h"
+#include "../include/gym.h"
+#include "../include/neuralnetwork.h"
+Image image;
+void generateData(nn::DataSet &ds)
+{
+    image = LoadImage("./data/9.png"); // Load image data into CPU memory (RAM)
+
+    Color *colors = LoadImageColors(image);
+    for (int y = 0; y < image.height; y++, std::cout << '\n')
+        for (int x = 0; x < image.width; x++)
+        {
+            int index = (y * image.width) + x;
+            int nr = 3;
+            nn::T val = colors[index].r;
+            if (val != 0)
+                nr = 3 - (int)std::log10(val);
+            std::cout << val;
+            for (int i = 0; i < nr; i++)
+                std::cout << " ";
+            std::cout << " ";
+
+            std::vector<nn::T> input(2);
+            input[0] = float(x) / (image.width - 1.0);
+            input[1] = float(y) / (image.height - 1.0);
+
+            std::vector<nn::T> output(1);
+            output[0] = (nn::T)val / 255.0;
+            ds.addData(input, output);
+        }
+}
+void display(nn::NeuralNetwork &n)
+{
+    std::cout << "\n";
+    for (int y = 0; y < image.height; y++, std::cout << '\n')
+        for (int x = 0; x < image.width; x++)
+        {
+            nn::Matrix input{1, 2};
+            input(0, 0) = nn::T(x) / (image.width - 1.0);
+            input(0, 1) = nn::T(y) / (image.height - 1.0);
+            nn::Matrix output = n.forward(input);
+            auto val = (int)(output(0, 0) * 255.0);
+            int nr = 3;
+            if (val != 0)
+                nr = 3 - (int)std::log10(val);
+            std::cout << val;
+            for (int i = 0; i < nr; i++)
+                std::cout << " ";
+            std::cout << " ";
+        }
+}
+int main()
+{
+    nn::NeuralNetwork n{{2, 28, 1}};
+    n.rand();
+    n.nrSamples = 2 * 28;
+
+    nn::DataSet ds;
+    generateData(ds);
+    ds.shuffle();
+
+    nn::Gym gym(n);
+    gym.train(ds, 30'000);
+
+    display(n);
+}
