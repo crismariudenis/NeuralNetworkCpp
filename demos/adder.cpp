45751075fefdfb5fd2472a6994b903a6e3db594d
crismariudenis <78813212+crismariudenis@users.noreply.github.com>
2023-08-29 18:31:19 +0300

    numbers lenght changeable

--- demos/adder.cpp
+++ demos/adder.cpp
@@ -4,51 +4,64 @@
 
 typedef double T;
 
-int main()
+size_t numbersLength = 3; // number of bits for each number
+size_t maxNumber = 1 << numbersLength;
+void generateData(nn::DataSet &ds)
 {
-    //---- Setup network and data -------
-    nn::NeuralNetwork n{{8, 16, 5}};
-
-    n.rate = 1;
-    n.nrSamples = 10;
-
-    n.rand();
-    nn::DataSet ds;
-    
-    ///-----------------------------------
-    for (int i = 0; i < 60; i++)
+    for (int i = 0; i < 20; i++)
     {
-        int a = rand() % 8;
-        int b = rand() % 8;
+        int a = rand() % maxNumber;
+        int b = rand() % maxNumber;
         std::vector<T> input;
         // add a and b 's bits
 
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < numbersLength; j++)
             input.push_back((a >> j) & 1);
 
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < numbersLength; j++)
             input.push_back((b >> j) & 1);
 
         std::vector<T> output;
         // add a + b 's bits
-        for (int j = 0; j < 5; j++)
+        for (int j = 0; j < numbersLength + 1; j++)
             output.push_back(((a + b) >> j) & 1);
         ds.addData(input, output);
     }
-    ///-----------------------------------
+}
+int main()
+{
+    //---- Setup network and data -------
+    nn::NeuralNetwork n{{2 * numbersLength, 4 * numbersLength, numbersLength + 1}};
+    n.rand();
+
+    nn::DataSet ds;
+    generateData(ds);
 
     nn::Gym gym(n);
-    gym.train(ds, 100000);
+    gym.train(ds, 50'000);
 
     // ---------------------PRINT-------------------------
     for (size_t i = 0; i < ds.size(); i++)
     {
         nn::Matrix acc = n.forward(ds.getInputMat(i));
-        std::vector<T> exp = ds.getData(i).output;
-#define test ds.getInputMat(i).data
-        std::cout << std::fixed << test[0] + 2 * test[1] + 4 * test[2] + 8 * test[3] + 16 * test[4] << " + "
-                  << test[4] + 2 * test[5] + 4 * test[6] + 8 * test[7] + 16 * test[8] << " = " << acc.data[0] + 2 * acc.data[1] + 4 * acc.data[2] + 8 * acc.data[3] + 16 * acc.data[4] + 32 * acc.data[5] << '\n';
+        auto value = ds.getInputMat(i).data;
+        size_t p = 1;
+
+        double nr = 0;
+        for (size_t i = 0, p = 1; i < numbersLength; i++, p *= 2)
+            nr += value[i] * p;
+        std::cout << nr << " + ";
+
+        nr = 0;
+        for (size_t i = 0, p = 1; i < numbersLength; i++, p *= 2)
+            nr += value[i + numbersLength] * p;
+        std::cout << nr << " = ";
+
+        nr = 0;
+        for (size_t i = 0, p = 1; i < numbersLength + 1; i++, p *= 2)
+            nr += acc.data[i] * p;
+        std::cout << nr << '\n';
     }
-    n.printWeights();
-    n.printBiases();
+    // n.printWeights();
+    // n.printBiases();
 }
